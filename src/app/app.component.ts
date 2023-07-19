670d2bf6c4fa27cbdff84da17514b08c6c4dd40c
Abhishek Magdum <abhishek.magdum.07@gmail.com>
2023-07-19 19:17:34 +0530

    Added : Curd Application using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {AfterViewInit} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crud-app';
+export class AppComponent implements OnInit{
+ 
+
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _dialog: MatDialog, private _empService: EmployeeService){}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+  openAddEditEmpForm(){
+      this._dialog.open(EmpAddEditComponent)
+  }
+
+  getEmployeeList(){
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    })
+  }
+
 }
